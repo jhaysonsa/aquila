a3e5950835ee7dde3e14d0aa22f109fe5f1d898b
jason ancheta <jasonmendozaancheta@gmail.com>
2024-03-26 05:14:19 +0800

    stylesheet equiue scripte

--- header.php
+++ header.php
@@ -14,6 +14,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <?php wp_head();  ?>
 </head>
-<body <?php body_class(); ?>>
+<body <?php body_class('hello-class'); ?>>
 <?php wp_body_open(); ?>
     <header>Header</header>
