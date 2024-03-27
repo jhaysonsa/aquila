2dbd10dfda3a070169b73ed6f4fe010e15505012
jason ancheta <jasonmendozaancheta@gmail.com>
2024-03-27 09:55:57 +0800

    update content

--- index.php
+++ index.php
@@ -12,17 +12,59 @@ get_header();
     <div class="primary">
         <main id="main" class="site-main mt-5" role="main">
         <?php
-          if (have_posts()) {
+          if (have_posts()) :
               ?>
               <div class="container" >
-                  <?php
-                   while( have_posts()) : the_post();
-                   the_title();
-                   the_content();
-                   endwhile;
-                  ?>
+                    <?php
+                     if(is_home() && ! is_front_page()) {
+                         ?>
+                         <header class="mb-5">
+                             <h1 class="page-title ">
+                                 <?php single_post_title();?>
+                             </h1>
+                         </header>
+                     <?php }    ?>
+
+                 <div class="row">
+                     <?php
+                     $index=0;
+                     $no_of_columns = 3;
+                     //start the loop
+                     while( have_posts()) : the_post();
+
+                     if(0 === $index % $no_of_columns) {
+                         ?>
+                         <div class="col-lg-4 col-md-6 col-sm-12">
+                        <?php
+                     }
+                      ?>
+
+
+
+                         <?php
+                             get_template_part('template-parts/content');
+                         $index++;
+
+                     if(0 !== $index && 0 === $index % $no_of_columns){
+                         ?>
+                         </div>
+                   <?php
+                     }
+                     endwhile;
+                     ?>
+
+                 </div>
               </div>
-          <?php } ?>
+          <?php
+
+
+          else:
+
+               get_template_part('template-parts/content-none');
+
+          endif;
+
+          ?>
         </main>
     </div>
 
